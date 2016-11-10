cf665a3276ce8f2aa6bc171feb292bf9b98c3f92
brett-smith <brett@hypersocket.com>
2016-11-10 12:38:01 +0000

    * Fixed javadoc compile and headers. * Merged bits from master * Bump to 3.0.0-SNAPSHOT so HS can use new terminal components.

--- src/main/java/com/sshtools/ui/awt/grid/Grid.java
+++ src/main/java/com/sshtools/ui/awt/grid/Grid.java
@@ -1,72 +1,17 @@
-/*
- * Copyrights and Licenses
- * 
- * This product includes Hypersonic SQL. Originally developed by Thomas Mueller
- * and the Hypersonic SQL Group.
- * 
- * Copyright (c) 1995-2000 by the Hypersonic SQL Group. All rights reserved.
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met: -
- * Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer. - Redistributions in binary
- * form must reproduce the above copyright notice, this list of conditions and
- * the following disclaimer in the documentation and/or other materials provided
- * with the distribution. - All advertising materials mentioning features or use
- * of this software must display the following acknowledgment: "This product
- * includes Hypersonic SQL." - Products derived from this software may not be
- * called "Hypersonic SQL" nor may "Hypersonic SQL" appear in their names
- * without prior written permission of the Hypersonic SQL Group. -
- * Redistributions of any form whatsoever must retain the following
- * acknowledgment: "This product includes Hypersonic SQL." This software is
- * provided "as is" and any expressed or implied warranties, including, but not
- * limited to, the implied warranties of merchantability and fitness for a
- * particular purpose are disclaimed. In no event shall the Hypersonic SQL Group
- * or its contributors be liable for any direct, indirect, incidental, special,
- * exemplary, or consequential damages (including, but not limited to,
- * procurement of substitute goods or services; loss of use, data, or profits;
- * or business interruption). However caused any on any theory of liability,
- * whether in contract, strict liability, or tort (including negligence or
- * otherwise) arising in any way out of the use of this software, even if
- * advised of the possibility of such damage. This software consists of
- * voluntary contributions made by many individuals on behalf of the Hypersonic
- * SQL Group.
- * 
- * 
- * For work added by the HSQL Development Group:
- * 
- * Copyright (c) 2001-2002, The HSQL Development Group All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- * Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer, including earlier license
- * statements (above) and comply with all above license conditions.
- * 
- * Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution, including earlier
- * license statements (above) and comply with all above license conditions.
- * 
- * Neither the name of the HSQL Development Group nor the names of its
- * contributors may be used to endorse or promote products derived from this
- * software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL HSQL DEVELOPMENT GROUP, HSQLDB.ORG, OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
- * Work added by 3SP <a href="http://3sp.com">http://3sp.com</a> for 
- * AWT UI components are licensed under the GPL.
- * LICENSE.txt in the root of this module. 
+/**
+ * SSHTOOLS Limited licenses this file to you under the Apache
+ * License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package com.sshtools.ui.awt.grid;
 
--- src/main/java/com/sshtools/ui/swing/XTextField.java
+++ src/main/java/com/sshtools/ui/swing/XTextField.java
@@ -1,24 +1,18 @@
-/*
- *  SSHTools - Java SSH2 API
+/**
+ * SSHTOOLS Limited licenses this file to you under the Apache
+ * License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License.
  *
- *  Copyright (C) 2002 Lee David Painter.
+ * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Library General Public License
- *  as published by the Free Software Foundation; either version 2 of
- *  the License, or (at your option) any later version.
- *
- *  You may also distribute it and/or modify it under the terms of the
- *  Apache style J2SSH Software License. A copy of which should have
- *  been provided with the distribution.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  License document supplied with your distribution for more details.
- *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
-
 package com.sshtools.ui.swing;
 
 import java.awt.Toolkit;
